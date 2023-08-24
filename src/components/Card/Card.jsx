8c902b35045589a0ca1dc2a8fbff62f4f199e557
unknown <soumy99sharma@gmail.com>
2023-08-25 00:37:38 +0530

    Added images

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-
+export const imageResourcePath = 'https://image.tmdb.org/t/p/w';
+export const imageSizeSmall = 200;
 function Card({ imageSrc, title, rating, description, id }) {
   return (
     <Link to={`/details/${id}`}>
-      <picture>
-        <source srcSet={imageSrc} />
-      </picture>
+      <img src={`${imageResourcePath}${imageSizeSmall}${imageSrc}`} alt={title} />
       <h3>{title}</h3>
       <span>{rating}</span>
       <p>{description}</p>
